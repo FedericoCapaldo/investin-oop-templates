b98b4a67ff664d11af520ebbbec8ea3dca0730a0
Federico Capaldo <federico.capaldo1@gmail.com>
2022-07-29 19:24:01 +0100

    inital commit

---
+++ date-template.py
@@ -0,0 +1,26 @@
+# DIFFICULTY LEVEL: Easy (compared to the other exerices, but it's still challenging!)
+# SUMMARY: Create a DateStamp class which takes day, month, and year as properties.
+#  Then create an object with a date of your choice and print it back with a fucntion.
+
+
+# 1. create DateStamp class
+# 1.1 the class should should be initialized with the following properties (all received at object creation)
+#   day
+#   month
+#   year
+# 1.2 create a function called get_date(self) in the DateStamp class that return the date in the format dd/mm/yyyy
+#   hint: str(number) converts a number to string, you may need this to convert the day, month, year from numbers to string in order to print correctly.
+# 1.3 create a function called set_date(self, day, month, year) in the DateStamp class to set a new day, month, year in its respective properties
+
+
+if __name__ == "__main__":
+    print("hello, world!")
+
+    # 2. create a DateStamp object called dateOne
+    #  (initialize the object with day, month, year of your choice)
+
+    # 3. print the date of dateOne
+
+    # 4. give dateOne a new day, month, year
+
+    # 5. print again the date of dateOne
---
+++ eye-sight-template.py
@@ -0,0 +1,42 @@
+# DIFFICULTY LEVEL: Medium
+# SUMMARY: Create a Customer class and EyesightTest class, so that each customer
+#  can keep track of their eyesight appointment history.
+# IMPORTANT: Follow the correct order of the instructions to create you program successfully!
+
+
+# 1. Create a Customer class
+# 1.1 the class should be initialized with the following properties (only name is received during object creation)
+#   name
+#   eye_sight_visits (just an empty list)
+# 1.2 create a function called say_hello(self) in the Customer class that prints "Hi, my name is" + the name of customer
+# 6. create a function in the Customer class called 'save_eyesight_test(self, new_test)' that stores a new test in the eye_sight_visits property
+# 8. create a function that returns all the eyesight tests (i.e. eye_sight_visits)
+
+
+# 3. Create class EyesightTest
+# 3.1 the class should be initialized with the following properties (all received at object creation)
+#   short_sightedness (it is measured from 0 - perfert eye - until -10.00)
+#   astigmatism (it is measured from 0 - perfect eye - until 20.00)
+#   test_date (e.g. '15/04/2020')
+# 3.2 create a function in the EyesightTest class that returns a summary of the test with all it's info
+
+
+if __name__ == "__main__":
+    print("hello, world")
+
+    # 2. create a customer object jenny
+    #  initialize this object with name 'jenny'
+
+    # 2.1 make jenny say hello (this should print out on the screen)
+
+    # 4. create and EyeTest object test1
+    # initialize this object with the desired values (hopefully our customer has good eyesight!),
+    # the date of the test should be sometime in 2020
+
+    # 5. create and EyeTest object test2
+    # initialize this object with the desired values (perhaps our customer sight worsened a bit!),
+    # the date of the test should be sometime in 2022
+
+    # 7. add both tests to the Customer object jenny
+
+    # 9. print a summary of each test for the customer jenny
