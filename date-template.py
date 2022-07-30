3680b47295dd5424b0fa2cad7baa31d531687e7e
Federico Capaldo <federico.capaldo1@gmail.com>
2022-07-30 03:36:41 +0100

    updated minor info and indentation

--- date-template.py
+++ date-template.py
@@ -10,17 +10,17 @@
 #   year
 # 1.2 create a function called get_date(self) in the DateStamp class that return the date in the format dd/mm/yyyy
 #   hint: str(number) converts a number to string, you may need this to convert the day, month, year from numbers to string in order to print correctly.
-# 1.3 create a function called set_date(self, day, month, year) in the DateStamp class to set a new day, month, year in its respective properties
+# 4. create a function called set_date(self, new_day, new_month, new_year) in the DateStamp class to set a new day, month, year in its respective properties
 
 
 if __name__ == "__main__":
-    print("hello, world!")
+  print("hello, world!")
 
-    # 2. create a DateStamp object called dateOne
-    #  (initialize the object with day, month, year of your choice)
+  # 2. create a DateStamp object called dateOne
+  #  (initialize the object with day, month, year of your choice)
 
-    # 3. print the date of dateOne
+  # 3. print the date of dateOne
 
-    # 4. give dateOne a new day, month, year
+  # 5. give dateOne a new day, month, year
 
-    # 5. print again the date of dateOne
+  # 6. print again the date of dateOne
