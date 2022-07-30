ee005e2dd772bd85244a9f0aa84ffa8cb88b1035
Federico Capaldo <federico.capaldo1@gmail.com>
2022-07-30 04:01:10 +0100

    updated minor info and indentation

--- eye-sight-template.py
+++ eye-sight-template.py
@@ -1,6 +1,5 @@
 # DIFFICULTY LEVEL: Medium
-# SUMMARY: Create a Customer class and EyesightTest class, so that each customer
-#  can keep track of their eyesight appointment history.
+# SUMMARY: Create a Customer class and EyesightTest class, so that each customer can keep track of their eyesight appointment history.
 # IMPORTANT: Follow the correct order of the instructions to create you program successfully!
 
 
@@ -10,7 +9,8 @@
 #   eye_sight_visits (just an empty list)
 # 1.2 create a function called say_hello(self) in the Customer class that prints "Hi, my name is" + the name of customer
 # 6. create a function in the Customer class called 'save_eyesight_test(self, new_test)' that stores a new test in the eye_sight_visits property
-# 8. create a function that returns all the eyesight tests (i.e. eye_sight_visits)
+#  hint: use the function .append() on a list to add a new value to it (i.e. in our case this means self.eye_sight_visits.append(new_test) )
+# 8. create a function that returns all the eyesight tests (i.e. self.eye_sight_visits)
 
 
 # 3.0 Create class EyesightTest
@@ -22,21 +22,21 @@
 
 
 if __name__ == "__main__":
-    print("hello, world")
+  print("hello, world")
 
-    # 2.0 create a customer object jenny
-    #  initialize this object with name 'jenny'
+  # 2.0 create a customer object jenny
+  #  initialize this object with name 'jenny'
 
-    # 2.1 make jenny say hello (verify that this prints out on the screen)
+  # 2.1 make jenny say hello (this should print out on the screen)
 
-    # 4. create and EyeTest object test1
-    # initialize this object with the desired values (hopefully our customer has good eyesight!),
-    # the date of the test should be sometime in 2020
+  # 4. create and EyesightTest object test1
+  # initialize this object with the desired values (hopefully our customer has good eyesight!),
+  # the date of the test should be sometime in 2020
 
-    # 5. create and EyeTest object test2
-    # initialize this object with the desired values (perhaps our customer sight worsened a bit!),
-    # the date of the test should be sometime in 2022
+  # 5. create and EyeTest object test2
+  # initialize this object with the desired values (perhaps our customer sight worsened a bit!),
+  # the date of the test should be sometime in 2022
 
-    # 7. add both tests to the Customer object jenny
+  # 7. add both tests to the Customer object jenny
 
-    # 9. print a summary of each test for the customer jenny
+  # 9. print a summary of each test for the customer jenny (hint: you should use a for loop)
